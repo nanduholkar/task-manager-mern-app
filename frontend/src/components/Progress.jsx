90763e21c51c4a60ce684a2f86d424276a9e1085
nanduholkar <nanduholkar7@gmail.com>
2026-06-05 21:39:01 +0530

    Add Progress component to display task progress with dynamic styling

---
+++ frontend/src/components/Progress.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Progress = ({progress, status}) => {
+
+    const getColor = () => {
+        switch(status){
+            case "in-progress":
+                return "text-cyan-500 bg-cyan-500 border border-cyan-500/10"
+            case "completed":
+                return "text-indigo-500 bg-indigo-500 border border-indigo-500/20"
+            default: 
+            return "text-violet-500 bg-violet-500 border border-violet-500/10"
+        }
+    }
+  return (
+    <div className='w-full bg-gray-200 rounded-full h-1.5'>
+      <div className={`${getColor()} h-1.5 rounded-full text-center text-xs font-medium`} style={{width: `${progress}%`}} ></div>
+    </div>
+  )
+}
+
+export default Progress
