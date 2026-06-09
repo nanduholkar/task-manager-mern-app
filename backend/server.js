b0e655c5c5a5c7cf5dfcbc2e2e778ca110ce8fda
nanduholkar <nanduholkar7@gmail.com>
2026-06-09 19:06:07 +0530

    Fix route path for reports API endpoint

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ connectDB()
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/tasks", taskRoutes)
-app.use("/api/report", reportRoutes)
+app.use("/api/reports", reportRoutes)
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
 // Start Server
