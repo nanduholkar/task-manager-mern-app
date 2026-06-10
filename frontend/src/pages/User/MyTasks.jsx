6f9ef9a80e7c78109cfcfae8d515a5157e58281f
nanduholkar <nanduholkar7@gmail.com>
2026-06-10 22:09:56 +0530

    Refactor MyTasks component to fetch and display tasks with status summary and improve UI with TaskStatusTabs

--- frontend/src/pages/User/MyTasks.jsx
+++ frontend/src/pages/User/MyTasks.jsx
@@ -1,11 +1,113 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DashboardLayout from "../../components/layout/DashboardLayout"
+import { useNavigate } from 'react-router-dom'
+import axiosInstance from '../../utils/axiosInstance'
+import { API_PATHS } from '../../utils/apiPaths'
+import { LuFileSpreadsheet } from "react-icons/lu";
+import  TaskStatusTabs from "../../components/TaskStatusTabs";
+import  TaskCard from "../../components/Cards/TaskCard";
 
-function ComponentName() {
+
+const MyTasks = () => {
+
+  const [allTasks, setAllTasks] = useState([])
+  const [tabs, setTabs] = useState([])
+  const [filterStatus, setFilterStatus] = useState("All")
+  
+  const navigate = useNavigate()
+
+  const getAllTasks = async (status) => {
+    try{
+      const responce = await axiosInstance.get(API_PATHS.TASKS.GET_ALL_TASKS,
+        {params:{
+          status: filterStatus === "All" ? "" : filterStatus
+        }
+    })
+    setAllTasks(responce.data?.tasks?.length > 0 ? responce.data.tasks : [])
+    
+    const statusSummary = responce.data?.statusSummary || {}
+
+    const statusArray = [
+    {
+    label: "All",
+    value: "All",
+    count: statusSummary.all || 0
+    },
+    {
+    label: "Pending",
+    value: "pending",
+    count: statusSummary.pendingTasks || 0
+    },
+    {
+      label: "In Progress",
+    value: "in-progress",
+    count: statusSummary.inProgressTasks || 0
+    },
+    {
+    label: "Completed",
+    value: "completed",
+    count: statusSummary.completedTasks || 0
+    }
+    ];
+
+    setTabs(statusArray)
+
+    }catch(error){
+      console.error("Error fetching users:", error)
+    }
+  }
+
+  const handleClick = (taskId) => {
+    navigate(`/user/task-details/${taskId}`)
+  }
+
+
+
+  useEffect(() => {
+    getAllTasks(filterStatus)
+    return () => {}
+  }, [filterStatus])
+
   return (
-    <div>
-      
-    </div>
+    <DashboardLayout activeMenu="My Tasks">
+      <div className='my-5'>
+        <div className='flex flex-col lg:flex-row lg:items-center justify-between'>
+          <h2 className='text-xl md:text-xl font-medium'>My Tasks</h2>
+          
+
+          {tabs?.[0]?.count > 0 && (
+            <TaskStatusTabs 
+              tabs={tabs}
+              activeTab={filterStatus}
+              setActiveTab={setFilterStatus}
+            />
+          )}
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
+          {allTasks?.map((item, idx) => (
+            <TaskCard
+              key={item._id}
+              title={item.title}
+              description={item.description}
+              priority={item.priority}
+              status={item.status}
+              progress={item.progress}
+              createdAt={item.createdAt}
+              dueDate={item.dueDate}
+              assignedTo={item.assignedTo?.map((item) => item.profileImageUrl)}
+              attachmentCount={item.attachment?.length || 0}
+              completedTodoCount={item.completedTodoCount || 0}
+              todoChecklist={item.todoChecklist || []}
+              onClick={() => {
+                handleClick(item._id)
+              }}
+            />
+          ))}
+        </div>
+      </div>
+    </DashboardLayout>
   )
 }
 
-export default ComponentName
+export default MyTasks
